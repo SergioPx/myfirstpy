c4f115c1bcc418112b9525d833119e457472a7a4
Sergio Ponce <sergio.poncet@gmail.com>
2021-06-21 13:02:47 +0200

    Working version

--- myfirstpy/lib.py
+++ myfirstpy/lib.py
@@ -1,25 +1,9 @@
-import vlc
-import pafy
+import webbrowser
+
 
 def try_me():
-    '''
-    Docstring obligado 
-    '''
+    return webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 
-    url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-    # url of the video
-
-    
-    # creating pafy object of the video
-    video = pafy.new(url)
-    
-    # getting best stream
-    best = video.getbest()
-    
-    # creating vlc media player object
-    media = vlc.MediaPlayer(best.url)
-    
-    # start playing video
-    media.play()
-    
 try_me()
+
+
